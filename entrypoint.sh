10cce79ec97c599ce927392f5ba6fd9a6b2a8735
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2022-02-11 17:03:33 +0100

    Enrich jetty config, based on the config used on georchestra

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@
 # Initialize the datadir content if not done (Windows doesn't)
 if [ -d "/mnt/geoserver_datadir" ] ; then
 	echo "checking if /mnt/geoserver_datadir need init"
-	if [ ! "$(ls -A /mnt/geoserver_datadir)" ] ;then
+	if [ -f /mnt/geoserver_datadir/global.xml ]
+	then
+	    echo 'Datadir already initialized'
+	else
 		echo "Initializing /mnt/geoserver_datadir"
 		cp -r $JETTY_BASE/webapps/${WEBAPP_PATH}/data/* /mnt/geoserver_datadir
 
@@ -12,9 +15,13 @@ if [ -d "/mnt/geoserver_datadir" ] ; then
 		if [[ -n $PROXY_BASE_URL ]]; then
 			sed -i "s|</settings>|<proxyBaseUrl>$PROXY_BASE_URL</proxyBaseUrl></settings>|" /mnt/geoserver_datadir/global.xml
 		fi
-	else
-		echo "already initialized"
 	fi
+
+  echo 'Change log config according to docker setup'
+  sed -i 's:stdOutLogging>false:stdOutLogging>true:g' /mnt/geoserver_datadir/logging.xml
+
+  echo 'deactivate oshi logs (hardware monitoring, fails on docker, the error logs pollute the geoserver logs)'
+  echo "log4j.category.oshi.hardware.platform.linux=OFF" >> /mnt/geoserver_datadir/logs/PRODUCTION_LOGGING.properties
 else
 	echo "/mnt/geoserver_datadir does not exist"
 fi
